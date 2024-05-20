8d3b43bc479c2bf0764e66aa04d7993436e83f00
SagarSingh2003 <iamsagar762@gmail.com>
2024-05-20 12:27:46 +0530

    modified check permissions and config.toml

--- supabase/functions/check-permission/index.ts
+++ supabase/functions/check-permission/index.ts
@@ -2,19 +2,20 @@ import { Permit } from "npm:permitio";
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': "*",
-  'Access-Control-Allow-Headers': 'Authorization, x-client-info, apikey, Content-Type',
-  'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'POST',
 }
+
 
 Deno.serve(async (req) => {
   
 
-  const { id , operation , resource , tenant  , permit_token} = await req.json()
+  const { user , action , resource , context} = await req.json()
 
   const permit = new Permit({
     // your API Key
-    token: permit_token,
-    pdp: "https://cloudpdp.api.permit.io",
+    token: Deno.env.get("PERMIT_TOKEN"),
+    pdp: Deno.env.get("PDP_URL"),
 
   });
 
@@ -27,10 +28,23 @@ Deno.serve(async (req) => {
 
   try{
 
-      const permitted = await permit.check( String(id) , String(operation) , {
-          type: resource ,
-          tenant: tenant ,
-        });
+      const permitted = await permit.check( 
+        
+        //id of the user
+        String(user),
+
+        //action 
+        String(action), 
+        
+        //resource
+        {
+          type: resource.type ,
+          tenant: resource.tenant ,
+        }, 
+        
+        //context
+        context
+      );
 
       console.log(permitted);
 
